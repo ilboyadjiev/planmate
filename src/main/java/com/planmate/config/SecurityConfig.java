7a8409215706b3fb60e7d0de03217634d0dd86d2
Ilian Boyadjiev <ilianboyadjiev1@gmail.com>
2026-03-19 19:01:44 +0100

    allow /error and /health endpoints

--- src/main/java/com/planmate/config/SecurityConfig.java
+++ src/main/java/com/planmate/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -52,6 +53,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			http.addFilterBefore(authRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		}
 	}
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/health", "/api/health/**", "/error");
+    }
 
     @Bean
     public CorsFilter corsFilter() {
